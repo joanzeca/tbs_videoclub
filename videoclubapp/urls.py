f7d5eb1cba6fca44da1812ee92370bc2d5161ca3
Joanzeca <joseantoniozc2001@gmail.com>
2024-06-23 15:55:02 -0500

    Se agregó la funcionalidad de editar una persona y eliminar una persona

--- videoclubapp/urls.py
+++ videoclubapp/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.inicio, name="inicio"),
     path("personas/", views.personas, name="personas"),
     path("contacto/", views.contacto, name="contacto"),
-    path("registrar-persona/", views.registrar_persona, name="registrar-persona"),
+    path("personas/registrar/", views.registrar_persona, name="registrar_persona"),
     path("success/", views.success, name="success"),
+    path('personas/editar/<int:id>/', views.editar_persona, name='editar_persona'),
+    path('personas/eliminar/<int:id>/', views.eliminar_persona, name='eliminar_persona'),
 ]
--- videoclubapp/views.py
+++ videoclubapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Persona
 from django.utils.timezone import datetime
+from django.http import HttpResponse
 
 # tarea ...
 def inicio(request):
@@ -53,3 +54,45 @@ def registrar_persona(request):
             return render(request, 'pages/registrar-persona.html', {"valor":valor})
         
     return render(request, 'pages/registrar-persona.html', {"valor":valor})
+
+def editar_persona(request, id):
+    persona = get_object_or_404(Persona, id=id)
+    fecha_de_nacimiento_formateada = persona.fecha_de_nacimiento.strftime('%Y-%m-%d')
+    if request.method == 'POST':
+        
+        campos = {
+            'firstnames': request.POST.get('firstnames').strip(),
+            'surnames': request.POST.get('surnames').strip(),
+            'address': request.POST.get('address').strip(),
+            'date': request.POST.get('date').strip(),
+            'age': request.POST.get('age').strip(),
+            'salary': request.POST.get('salary').strip(),
+            'rnd': request.POST.get('rnd').strip(),
+            'state': request.POST.get('state').strip(),
+        }
+
+        campos_invalidos = [nombre for nombre, valor in campos.items() if not valor]
+
+        if len(campos_invalidos) > 0:
+            valor = "invalido"
+            return render(request, 'pages/registrar-persona.html', {"valor":valor})
+        else:
+            valor = "valido"
+            persona.nombres = campos["firstnames"]
+            persona.apellidos = campos["surnames"]
+            persona.direccion = campos["address"]
+            persona.fecha_de_nacimiento = datetime.strptime(campos["date"], '%Y-%m-%d').date()
+            persona.edad = int(campos["age"])
+            persona.sueldo = float(campos["salary"])
+            persona.rnd = campos["rnd"]
+            persona.estado = campos["state"]
+            persona.save()
+            return redirect("personas")
+        # return HttpResponse("POST")
+    else:
+        return render(request, "pages/editar_persona.html", {"persona":persona, "fecha_de_nacimiento_formateada":fecha_de_nacimiento_formateada})
+
+def eliminar_persona(request, id):
+    persona = get_object_or_404(Persona, id=id)
+    persona.delete()
+    return redirect("personas")
